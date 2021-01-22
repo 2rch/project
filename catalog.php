ce84ab91633b10a84f5e3894a4e98cc0f2ff7815
Alex Turch <alexturch97@gmail.com>
2021-01-22 10:10:35 +0200

    Add a catalog

---
+++ catalog.php
@@ -0,0 +1,81 @@
+<?php include("header.php"); ?>
+<aside>
+    <div class="aside">
+        <h3>Категории</h3>
+        <ul>
+            <li><a href="">Casual тремпели</a></li>
+            <li><a href="">Дизайнерские тремпели</a></li>
+            <li><a href="">Вычурные тремпели</a></li>
+            <li><a href="">Тремпель на заказ</a></li>
+            <li><a href="">Ещё тремпель</a></li>
+        </ul>
+    </div>
+</aside>
+<main>
+    <div class="row">
+        <div class="col-sm-16">
+            <div class="product">
+                <div class="product-img">
+                    <a href="#"><img class="product-img" src="pics/c1.jpg" alt="Фото тремпеля"></a>
+                </div>
+                <p class="product-title">
+                    <a href="#">Элегантный тремпель</a>
+                </p>
+                <p class="product-desc">trempel.inc</p>
+                <p class="product-price">Цена: $10.00</p>
+            </div>
+            <div class="product">
+                <div class="product-img">
+                    <a href="#"><img class="product-img" src="pics/c3.png" alt="Фото тремпеля"></a>
+                </div>
+                <p class="product-title">
+                    <a href="#">Элегантный тремпель</a>
+                </p>
+                <p class="product-desc">trempel.inc</p>
+                <p class="product-price">Цена: $10.00</p>
+            </div>
+            <div class="product">
+                <div class="product-img">
+                    <a href="#"><img class="product-img" src="pics/c4.jpg" alt="Фото тремпеля"></a>
+                </div>
+                <p class="product-title">
+                    <a href="#">Элегантный тремпель</a>
+                </p>
+                <p class="product-desc">trempel.inc</p>
+                <p class="product-price">Цена: $10.00</p>
+            </div>
+            <div class="product">
+                <div class="product-img">
+                    <a href="#"><img class="product-img" src="pics/c1.jpg" alt="Фото тремпеля"></a>
+                </div>
+                <p class="product-title">
+                    <a href="#">Элегантный тремпель</a>
+                </p>
+                <p class="product-desc">trempel.inc</p>
+                <p class="product-price">Цена: $10.00</p>
+            </div>
+            <div class="product">
+                <div class="product-img">
+                    <a href="#"><img class="product-img" src="pics/c3.png" alt="Фото тремпеля"></a>
+                </div>
+                <p class="product-title">
+                    <a href="#">Элегантный тремпель</a>
+                </p>
+                <p class="product-desc">trempel.inc</p>
+                <p class="product-price">Цена: $10.00</p>
+            </div>
+            <div class="product">
+                <div class="product-img">
+                    <a href="#"><img class="product-img" src="pics/c4.jpg" alt="Фото тремпеля"></a>
+                </div>
+                <p class="product-title">
+                    <a href="#">Элегантный тремпель</a>
+                </p>
+                <p class="product-desc">trempel.inc</p>
+                <p class="product-price">Цена: $10.00</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</main>
+<?php include("footer.php"); ?>
