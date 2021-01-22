83478dc45a44fed520447b6366736af7054aadd3
Alex Turch <alexturch97@gmail.com>
2021-01-22 08:24:11 +0200

    Add main page

--- index.php
+++ index.php
@@ -1,4 +1,50 @@
-<?php
-// this line will print Holly molly
-echo 'Holy molly!';
-?>
+<?php include("header.php"); ?>
+<aside>
+    <div class="aside">
+        <h3>Категории</h3>
+        <ul>
+            <li><a href="">Casual тремпели</a></li>
+            <li><a href="">Дизайнерские тремпели</a></li>
+            <li><a href="">Вычурные тремпели</a></li>
+            <li><a href="">Тремпель на заказ</a></li>
+            <li><a href="">Ещё тремпель</a></li>
+        </ul>
+    </div>
+</aside>
+<main>
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="pics/c1.jpg" width="720" height="480" alt="Первый тремпель">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="pics/c3.png" width="720" height="480" alt="Второй тремпель">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="pics/c4.jpg" width="720" height="480" alt="Третий тремпель">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <article>
+    <h1>Lorem ipsum</h1>
+        <p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
+        <p>labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+        <p>laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in </p>
+        <p>voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </article>
+</main>
+<?php include("footer.php"); ?>
