ea2bd3472ed6c0985b293fef733cf5ffc50a60d2
Alex Turch <alexturch97@gmail.com>
2021-01-22 11:47:17 +0200

    Add shoping cart

--- cart.php
+++ cart.php
@@ -1,1 +1,61 @@
-<?php
+<?php include("header.php"); ?>
+<aside>
+    <div class="aside">
+        <h3>Категории</h3>
+        <ul>
+            <li><a href="">Casual тремпели</a></li>
+            <li><a href="">Дизайнерские тремпели</a></li>
+            <li><a href="">Вычурные тремпели</a></li>
+            <li><a href="">Тремпель на заказ</a></li>
+            <li><a href="">Ещё тремпель</a></li>
+        </ul>
+    </div>
+</aside>
+<main>
+<div class="row">
+    <ul>
+    <li style="list-style: none">
+        <div class="product-cart">
+            <img class="cart-pic" src="pics/c1.jpg"
+            <p class="cart-text">Элегантный тремпель</p>
+            <p class="cart-text">trempel.inc</p>
+            <p class="cart-text">Цена: $10.00</p>
+        </div>
+    </li>
+    <li>
+        <div class="product-cart">
+            <img class="cart-pic" src="pics/c1.jpg"
+            <p class="cart-text">Элегантный тремпель</p>
+            <p class="cart-text">trempel.inc</p>
+            <p class="cart-text">Цена: $10.00</p>
+        </div>
+    </li>
+    <li>
+        <div class="product-cart">
+            <img class="cart-pic" src="pics/c1.jpg"
+            <p class="cart-text">Элегантный тремпель</p>
+            <p class="cart-text">trempel.inc</p>
+            <p class="cart-text">Цена: $10.00</p>
+        </div>
+    </li>
+    <li>
+        <div class="product-cart">
+            <img class="cart-pic" src="pics/c1.jpg"
+            <p class="cart-text">Элегантный тремпель</p>
+            <p class="cart-text">trempel.inc</p>
+            <p class="cart-text">Цена: $10.00</p>
+        </div>
+    </li>
+    <li>
+        <div class="product-cart">
+            <img class="cart-pic" src="pics/c1.jpg"
+            <p class="cart-text">Элегантный тремпель</p>
+            <p class="cart-text">trempel.inc</p>
+            <p class="cart-text">Цена: $10.00</p>
+        </div>
+    </li>
+    </ul>
+    </div>
+</div>
+</main>
+<?php include("footer.php"); ?>
