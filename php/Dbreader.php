5e761995424aa3d71be0091a57fd4f4a26b31829
Alex Turch <alexturch97@gmail.com>
2021-01-27 10:24:09 +0200

    Add Dbreader, ID exception

---
+++ php/Dbreader.php
@@ -0,0 +1,23 @@
+<?php
+class Dbreader
+{
+    /**
+     * @var array
+     */
+    private $products;
+
+    public function __construct(array $products = [])
+    {
+        $this->products = $products;
+    }
+
+    public function getByID(int $id): array
+    {
+        foreach ($this->products as $product) {
+            if ($product["id"] == $id) {
+                return $product;
+            }
+        }
+        throw new IdException('Wrong ID');
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php require '../php/autoload.php';
 //$config = require '../conf/config.php';
-$renderer = new TemplateRender();
-$renderer->render('main', 'header', 'footer');
-//echo "I'm working";
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$products = require '../db/product-db.php';
+try
+{
+    $db = new Dbreader($products);
+    $product = $db->getByID(3);
+    echo implode($product);
+    $renderer = new TemplateRender();
+    $renderer->render('main', 'header', 'footer');
+}
+catch (\Exception $error){
+    echo $error;
+}
 
