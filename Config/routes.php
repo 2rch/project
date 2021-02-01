b9e039c980b75ec34375e79b9fef2f9b989fab9b
Alex Turch <alexturch97@gmail.com>
2021-02-01 10:47:21 +0200

    Add Router class

---
+++ Config/routes.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'main/show' => [
+      'controller' => 'main',
+    'action' => 'show'],
+    'login/login' => [
+    'controller' => 'login',
+    'action' => 'login'],
+];
---
+++ Controllers/Router.php
@@ -0,0 +1,26 @@
+<?php
+namespace controllers;
+class Router{
+    protected  $routes = [];
+    protected $params = [];
+    public function __construct(){
+        $arr = require 'Config/routes.php';
+        foreach ($arr as $key => $val){
+            $this->addRoute($key, $val);
+        }
+    }
+    public function addRoute($route, $params){
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $params;
+    }
+    public function match(){
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach($this->routes as $route => $params){
+            if (preg_match($route, $url, $matches)){
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+}
